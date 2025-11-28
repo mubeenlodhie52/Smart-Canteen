e1871bb0ec12d6c6b1056325d5acdae5db866de4
mubeenlodhie52 <mubeenlodhie52@gmail.com>
2025-11-28 17:25:23 +0500

    updated

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,3 +1,10 @@
+CREATE TABLE Admins (
+    admin_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL -- store hashed password
+);
+
+
 CREATE TABLE Users (
     user_id SERIAL PRIMARY KEY,
     name VARCHAR(100),
